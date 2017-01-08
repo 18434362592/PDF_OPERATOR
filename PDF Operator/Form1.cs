d150fdabd0e2efb7244764328fc53fcaf82bb5c2
刘洁 <18434362592@163.com>
2017-01-08 11:36:49 +0800

    处于实验阶段

---
+++ PDF Operator/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PDF_Operator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CrtButon_Click(object sender, EventArgs e)
+        {
+            if(PathTxt.Text==""||txtPath.Text=="")
+            {
+                MessageBox.Show("There are noting legal path!");
+            }
+            else
+            {
+                Functionality functionality = new Functionality(PathTxt.Text,txtPath.Text);
+            }
+        }
+    }
+}
